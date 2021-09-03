7f96d0fcccbc2148b7744b28aec826fed207ac05
Ken Sanchez <kensanchez16@gmail.com>
2021-09-03 02:39:44 -0500

    added form to add new tasks from ui react

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,28 +1,53 @@
-import React, {Component} from 'react';
-import TodoForm from './TodoForm';
+import React, { Component } from "react"
+import TodoForm from "./TodoForm"
+import TodoItem from "./TodoItem"
 
 const api_url = `http://localhost:3001/api/v1/todos`
 
-class TodoList extends Component{
-    constructor(props){
+class TodoList extends Component {
+    constructor(props) {
         super(props)
 
         this.state = {
             items: []
         }
+        this.updateTodoList = this.updateTodoList.bind(this);
+    }
+    componentDidMount() {
+        this.getTasks();
+
     }
 
-    render () {
+    getTasks() {
+        fetch(api_url)
+            .then(response => response.json())
+            .then(response_items => {
+                this.setState({
+                    items: response_items.reverse()
+                })
+            });
+    }
+
+    updateTodoList(item) {
+        let _items = this.state.items
+        _items.unshift(item)
+        this.setState({
+            items: _items
+        })
+    }
+
+    render() {
+        console.log(this.state.items)
         return (
             <div>
-                <TodoForm />
-                <ul>
-                    <li>Todo #1</li>
-                    <li>Todo #2</li>
+                <TodoForm api_url={api_url} updateTodoList={this.updateTodoList} />
+                <ul id="todo_list">
+                    {this.state.items.map((item) => (
+                        <TodoItem key={item.id} item={item} />
+                    ))}
                 </ul>
             </div>
         )
     }
 }
-
 export default TodoList;
